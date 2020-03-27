0b68a941ba00c01bd04ed3d4871c65f6b75411a7
Andre Gaillard <andre@gimalon.com>
2020-03-27 22:34:06 +0100

    initial commit

---
+++ frontend/src/appState.ts
@@ -0,0 +1,11 @@
+//import { observable, action } from 'mobx'
+//import bind from 'bind-decorator'
+//import AssistantIcon from '@material-ui/icons/Assistant'
+//import FitnessCenterIcon from '@material-ui/icons/FitnessCenter'
+// import LocalHospitalIcon from '@material-ui/icons/LocalHospital'
+//import { SvgIconProps } from '@material-ui/core'
+// import React, { FunctionComponentElement } from 'react'
+
+class AppState {}
+
+export const appState = new AppState()
