a0a322d01ebfa07cb3c0d411aadbdadaf9b1f3a9
Andre Gaillard <andre@gimalon.com>
2020-03-29 00:48:25 +0100

    bracket fix

--- server/src/API/QRcode.ts
+++ server/src/API/QRcode.ts
@@ -10,12 +10,12 @@ exports.check = function(req: Request, res: Response) {
     db_checkReservation(DB, store_id, reservation_id, function(result: any){
         res.end(JSON.stringify({'success': result}));
     });
-
-    exports.create = function(req: Request, res: Response) {
+}
+exports.create = function(req: Request, res: Response) {
         const storeID: number = parseInt(req.params.store_id)
         const date: string = req.params.date
         const time: string = req.params.time
-        db_makeReservation(DB, storeID, date, time, function(result: any) {
+        db_makeReservation(DB, storeID, date, time, ' ', function(result: any) {
           res.end(JSON.stringify({ success: result }))
         })
 }
