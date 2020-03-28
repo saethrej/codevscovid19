9f93fb4ca46195bb4b3a028f783608ecf370af3d
Andre Gaillard <andre@gimalon.com>
2020-03-29 00:01:01 +0100

    reservation

--- server/src/API/QRcode.ts
+++ server/src/API/QRcode.ts
@@ -2,7 +2,7 @@
 
 import { logger } from '../logger'
 import {Request, Response} from "express"
-import {db_checkReservation} from '../utils/db_ops'
+import {db_checkReservation, db_makeReservation} from '../utils/db_ops'
 
 exports.check = function(req: Request, res: Response) {
     let store_id:number = parseInt(req.params.store_id); //number
@@ -10,4 +10,12 @@ exports.check = function(req: Request, res: Response) {
     db_checkReservation(DB, store_id, reservation_id, function(result: any){
         res.end(JSON.stringify({'success': result}));
     });
+
+    exports.create = function(req: Request, res: Response) {
+        const storeID: number = parseInt(req.params.store_id)
+        const date: string = req.params.date
+        const time: string = req.params.time
+        db_makeReservation(DB, storeID, date, time, function(result: any) {
+          res.end(JSON.stringify({ success: result }))
+        })
 }
