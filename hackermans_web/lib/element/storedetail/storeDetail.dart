06219c8b36e038a541603082a139a8782fddb19d
Livio Schläpfer <livioschlapfer@Livios-MacBook-Pro.local>
2020-03-28 15:25:40 +0100

    update

--- hackermans_web/lib/element/storedetail/storeDetail.dart
+++ hackermans_web/lib/element/storedetail/storeDetail.dart
@@ -1,25 +1,32 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:hackermans_web/element/storedetail/storeInfo.dart';
 
 class StoreDetail extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Container(
-        width: MediaQuery.of(context).size.width / 2,
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: <Widget>[
-              Spacer(),
-              Text('data'),
-              Spacer(),
-            ],
-          ),
-        )
+      child: Card(
+        child: Container(
+          width: MediaQuery.of(context).size.width / 2,
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                Text('data'),
+                Row(
+                  children: <Widget>[
+                    StoreDetail(),
+                    StoreInfo(),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ),
       ),
     );
   }
---
+++ hackermans_web/lib/element/storedetail/storeInfo.dart
@@ -0,0 +1,27 @@
+
+
+import 'package:flutter/material.dart';
+
+class StoreInfo extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Card(
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                Spacer(),
+                Text('Store Info'),
+                Spacer(),
+              ],
+            ),
+          )
+        ),
+      ),
+    );
+  }
+}
