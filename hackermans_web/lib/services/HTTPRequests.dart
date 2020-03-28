7f3bbf9cbc24e7456b9fb75350e93363a928e098
andrega123 <andregaillard123@outlook.com>
2020-03-28 18:08:23 +0100

    extended http

--- hackermans_web/lib/services/HTTPRequests.dart
+++ hackermans_web/lib/services/HTTPRequests.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:core';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:tuple/tuple.dart';
 
@@ -52,14 +53,27 @@ class HTTPRequest {
     else {
       throw Exception("Failed to connect to server");
     }
-    
+  }
 
-    Future<List<DateTime>> requestTimes(int id, DateTime time) async{
+    Future<List<TimeOfDay>> requestTimes(int id, DateTime time) async{
       var jsonString = json.encode({'id': id, 'hour': time.hour,
       'minute': time.minute});
       final http.Response response = await http.post(server, headers: <String, String> {
       'Content-Type': 'application/json' },
       body: jsonString);
+      List<TimeOfDay> times = new List();
+  
+      if(response.statusCode == 201){
+        var timesNoFormat = jsonDecode(response.body)['times'];
+        for(var i = 0; i<timesNoFormat.length; i++){
+          times.add(new TimeOfDay(hour: timesNoFormat[i].hour, minute: timesNoFormat[i].minute));
+        }
+        return times;
+      }
+
+      else {
+        throw Exception ("Failed to connect to server");
+      }
 
 
 
@@ -67,5 +81,5 @@ class HTTPRequest {
 
 
 
-  }
+  
 }
