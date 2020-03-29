f9385844c025e4c6a0b08025fd0c258b36282700
Christopher <raffl@student.ethz.ch>
2020-03-29 16:34:05 +0200

    Resolved merging issues

--- server/src/API/QRcode.ts
+++ server/src/API/QRcode.ts
@@ -1,11 +1,8 @@
 //define database somehow
 
-/*
-
 import { logger } from '../logger'
 import {Request, Response} from "express"
 import {db_checkReservation, db_increase, db_getPeopleInStore} from '../utils/db_ops'
-
 
 /** brief: checks if hash of QR-code allows for entry. If yes, increments store counter
  * call with POST /checkQRCode
@@ -31,4 +28,3 @@ exports.check = function(req: Request, res: Response) {
         }
     })
 }
-
--- server/src/API/routes.ts
+++ server/src/API/routes.ts
@@ -1,27 +1,33 @@
-module.exports = function(app: any) {
-  var counter = require('./counter')
+module.exports = function (app: any) {
+    var counter = require('./counter')
 
-  // todoList Routes
+    // todoList Routes
 
-  app.route('/counterup/:storeId').get(counter.up)
+    app.route('/counterup/:storeId')
+        .get(counter.up)
 
-  app.route('/counterdown/:storeId').get(counter.down)
+    app.route('/counterdown/:storeId')
+        .get(counter.down)
 
-  app.route('/getcounter/:storeId').get(counter.value)
+    app.route('/getcounter/:storeId')
+        .get(counter.value)
 
-  var QRCode = require('./QRCode')
+    var QRCode = require('./QRCode')
 
-  app.route('/checkQRCode').post(QRCode.check)
+    app.route('/checkQRCode')
+        .post(QRCode.check)
+    
+    var stores = require('./stores')
 
-  var stores = require('./stores')
+    app.route('/getLocations')
+        .post(stores.locations)
 
-  app.route('/getLocations').post(stores.locations)
+    app.route('/getStoreData/:storeId')
+        .get(stores.dat)
 
-  app.route('/getStoreData/:storeId').get(stores.dat)
-  
-  var reservations = require('./reservations')
-  
-  app.route('/getavailableReservation')
+    var reservations = require('./reservations')
+
+    app.route('/getavailableReservation')
         .post(reservations.available)
 
     app.route('/reserveReservation')
@@ -30,4 +36,3 @@ module.exports = function(app: any) {
     app.route('/confirmReservation')
         .post(reservations.confirm)
 }
-
