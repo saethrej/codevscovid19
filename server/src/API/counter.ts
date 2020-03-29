0ecaa15a6e3e0432b18ba31ecbcd5f5af2d33470
Christopher <raffl@student.ethz.ch>
2020-03-29 20:05:45 +0200

    Added basic authorization for relevant calls and getnextReservation/:storeId

--- server/src/API/counter.ts
+++ server/src/API/counter.ts
@@ -2,7 +2,7 @@
 
 import { logger } from '../logger'
 import { Request, Response } from 'express'
-import { db_increase, db_decrease, db_getPeopleInStore } from '../utils/db_ops'
+import { db_increase, db_decrease, db_getPeopleInStore, db_checkCredentials } from '../utils/db_ops'
 
 /** brief: tries to increment the store counter and returns current value
  * call with GET /counterup/{id}
@@ -11,13 +11,29 @@ import { db_increase, db_decrease, db_getPeopleInStore } from '../utils/db_ops'
  * @param {Response} res contains JSON with field 'success' and 'people_in_store'
  * @returns -
  */
-exports.up = function(req: Request, res: Response) {
-  let id: number = parseInt(req.params.storeId)
-  db_increase(DB, id, function(result_1: any) {
-    db_getPeopleInStore(DB, id, function(result_2: any) {
-      res.end(JSON.stringify({ success: result_1, people_in_store: result_2 }))
+exports.up = function (req: Request, res: Response) {
+    let store_id: number = parseInt(req.params.storeId)
+    let pw = 'a7YLnpYh1b'
+    let username = '2coolMovie'
+    if (typeof req.get('user') !== 'undefined') {
+        username = req.get('user') + ''
+    }
+    if (typeof req.get('pw') !== 'undefined') {
+        pw = req.get('pw') + ''
+    }
+    logger.info(username)
+    db_checkCredentials(DB, store_id, username, pw, function (authenticated: boolean) {
+        if (authenticated) {
+            db_increase(DB, store_id, function (result_1: any) {
+                db_getPeopleInStore(DB, store_id, function (result_2: any) {
+                    res.end(JSON.stringify({ success: result_1, people_in_store: result_2 }))
+                })
+            })
+        }
+        else {
+            res.end(JSON.stringify({ 'success': false, 'error': 'not_authorized' }))
+        }
     })
-  })
 }
 
 /** brief: tries to decrement the store counter and returns current value
@@ -27,15 +43,30 @@ exports.up = function(req: Request, res: Response) {
  * @param {Response} res contains JSON with field 'success' and 'people_in_store'
  * @returns -
  */
-exports.down = function(req: Request, res: Response) {
-  let id: number = parseInt(req.params.storeId)
-  db_decrease(DB, id, function(result_1: any) {
-    db_getPeopleInStore(DB, id, function(result_2: any) {
-      res.end(JSON.stringify({ success: result_1, people_in_store: result_2 }))
+exports.down = function (req: Request, res: Response) {
+    let store_id: number = parseInt(req.params.storeId)
+    let pw = 'a7YLnpYh1b'
+    let username = '2coolMovie'
+    if (typeof req.get('user') !== 'undefined') {
+        username = req.get('user') + ''
+    }
+    if (typeof req.get('pw') !== 'undefined') {
+        pw = req.get('pw') + ''
+    }
+    logger.info(username)
+    db_checkCredentials(DB, store_id, username, pw, function (authenticated: boolean) {
+        if (authenticated) {
+            db_decrease(DB, store_id, function (result_1: any) {
+                db_getPeopleInStore(DB, store_id, function (result_2: any) {
+                    res.end(JSON.stringify({ success: result_1, people_in_store: result_2 }))
+                })
+            })
+        }
+        else {
+            res.end(JSON.stringify({ 'success': false, 'error': 'not_authorized' }))
+        }
     })
-  })
 }
-
 /** brief: returns current counter value of specified store
  * call with GET /getcounter/{id}
  *
@@ -43,10 +74,10 @@ exports.down = function(req: Request, res: Response) {
  * @param {Response} res contains JSON with field 'success' and 'people_in_store'
  * @returns -
  */
-exports.value = function(req: Request, res: Response) {
-  let id: number = parseInt(req.params.storeId)
-  let status = 'fail'
-  db_getPeopleInStore(DB, id, function(result: any) {
-    res.end(JSON.stringify({ success: true, people_in_store: result }))
-  })
+exports.value = function (req: Request, res: Response) {
+    let id: number = parseInt(req.params.storeId)
+    let status = 'fail'
+    db_getPeopleInStore(DB, id, function (result: any) {
+        res.end(JSON.stringify({ success: true, people_in_store: result }))
+    })
 }
